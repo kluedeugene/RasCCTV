b3a3436585d06216c22d9105c6b40b9b0449606c
kluedeugene <yujindd@gmail.com>
2021-10-28 18:49:15 +0900

    fcm 콘솔에서 전송시 notification body로 로그됨, python에서 전송시 data payload로 전송 받음. 알림은 표시되지않음.

--- app/src/main/java/com/company/RasCCTV/MyFirebaseMessaging.java
+++ app/src/main/java/com/company/RasCCTV/MyFirebaseMessaging.java
@@ -1,52 +1,119 @@
 package com.company.RasCCTV;
-import android.app.Notification;
+
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-
+import com.company.RasCCTV.MainActivity;
+import com.company.RasCCTV.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
+    private static final String TAG = "MyFirebaseMsgService";
 
-    private final String TAG= "MyFirebase";
+
 
     @Override
-    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
 
-        super.onMessageReceived(remoteMessage);
+    public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        Log.d(TAG, " onMessageReceived is called ");
-        String msg, title;
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
 
 
-        //FCM 메시지를 받고 노티피케이션 데이터가 있다면
-        if (remoteMessage.getNotification()!=null){
-            Log.d(TAG, "title : "+ remoteMessage.getNotification().getTitle());
-            Log.d(TAG, "body : "+ remoteMessage.getNotification().getBody());
-            msg= remoteMessage.getNotification().getBody();
-            title= remoteMessage.getNotification().getTitle();
-            Notification.Builder noti= new Notification.Builder(this)
-                    .setContentTitle("New push from : "+title)
-                    .setContentText(msg)
-                    .setSmallIcon(R.mipmap.ic_launcher);
 
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0,noti.build());
-            //제목과 내용외에 추가로 "키"와 "값"을 전송했따면 다음과 같은 방법으로 값을 가져올수있음.
-//            Log.d(TAG, "전송된값 : "+ remoteMessage.getData().get("여기에 전송한 key를 입력"));
+        if (remoteMessage.getData().size() > 0) {
+
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (true) {
+
+            } else {
+
+                handleNow();
+
+            }
+
+        }
+
+        if (remoteMessage.getNotification() != null) {
+
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+
+            sendNotification(remoteMessage.getNotification().getBody());
+
         }
 
     }
 
-    @Override
-    public void onNewToken(@NonNull String s){
-        super.onNewToken(s);
-        Log.d(TAG, "생성된 토큰 : "+s);
+    private void handleNow() {
+
+        Log.d(TAG, "Short lived task is done.");
+
+    }
+
+
+
+    private void sendNotification(String messageBody) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+
+                PendingIntent.FLAG_ONE_SHOT);
+
+
+
+        String channelId = getString(R.string.default_notification_channel_id);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder =
+
+                new NotificationCompat.Builder(this, channelId)
+
+                        .setSmallIcon(R.mipmap.ic_launcher)
+
+                        .setContentTitle("FCM Message")
+
+                        .setContentText(messageBody)
+
+                        .setAutoCancel(true)
+
+                        .setSound(defaultSoundUri)
+
+                        .setContentIntent(pendingIntent);
+
+
+
+        NotificationManager notificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            String channelName = getString(R.string.default_notification_channel_name);
+
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+        notificationManager.notify(0, notificationBuilder.build());
+
     }
 
 }
